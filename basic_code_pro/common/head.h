0e716d6d91881da07ea4f8659b771acfde347aea
LS-01 <LS_01_phil@163.com>
2021-04-13 23:59:05 +0800

    add basic_code_pro

---
+++ basic_code_pro/common/head.h
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: head.h
+	> Author: ls
+	> Mail: 
+	> Created Time: Tue 13 Apr 2021 11:14:59 PM CST
+ ************************************************************************/
+
+#ifndef _HEAD_H
+#define _HEAD_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+#endif
